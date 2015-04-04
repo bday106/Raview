845f2842aabbab54cdc45623f0f493db63f0a6e5
Joe DiMaria <joe.a.dimaria@gmail.com>
2015-04-04 15:29:46 -0700

    Added Bootstrap for review detail

--- client/parties/controllers/reviewDetails.js
+++
@@ -1,6 +0,0 @@
-angular.module("raview").controller("ReviewDetailsCtrl", ['$scope', '$stateParams', '$meteor',
-  function($scope, $stateParams, $meteor){
-
-    $scope.review = $meteor.object(Reviews, $stateParams.reviewId);
-
-}]);
--- client/parties/controllers/reviewsList.js
+++
@@ -1,9 +0,0 @@
-angular.module("raview").controller("ReviewsListCtrl", ['$scope', '$meteor',
-  function($scope, $meteor){
-
-    $scope.reviews = $meteor.collection(Reviews);
-
-    $scope.remove = function(party){
-      $scope.reviews.splice( $scope.reviews.indexOf(party), 1 );
-    };
-}]);
