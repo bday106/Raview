47d620a9402774e49702198a06223e703656a467
Joe DiMaria <joe.a.dimaria@gmail.com>
2015-04-04 22:00:37 -0700

    Added files

--- client/reviews/controllers/itemsList.js
+++
@@ -1,6 +0,0 @@
-angular.module("raview").controller("ItemsListCtrl", ['$scope', '$stateParams', '$meteor',
-  function($scope, $stateParams, $meteor){
-
-    $scope.reviews = $meteor.object(Reviews, $stateParams.reviewId);
-
-}]);
